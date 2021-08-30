efa85cd2eaec9e20bfca9a8119957b17787b6558
jirka88 <59512637+jirka88@users.noreply.github.com>
2021-08-30 17:24:49 +0200

    Add files via upload

---
+++ whack.js
@@ -0,0 +1,57 @@
+let count = 30;
+let Skore = 1;
+let nahoda = 0;
+var cass;
+let Mole;
+let prvniMole = -1;
+let startt = false;
+function start() {
+    if (!startt) {
+        cass = setInterval(cas, 1000)
+    }
+
+}
+function cas() {
+    startt = true;
+    count--;
+    document.querySelector(".cas").innerHTML = "Zbývajicí čas: " + count;
+    if (count === 0) {
+        clearInterval(cass);
+        clear();
+    }
+    else {
+        Mole = document.getElementsByClassName("mole")[random(nahoda)];   //výběr náhodného mole 
+        Mole.style.display = "block";
+        Mole.style.animation = "pohyb 0.4s 1"
+        Mole.style.top = "-70%";
+        setTimeout(zmiz, 650);      //po časovém intervalu zmizí 
+    }
+
+
+}
+function random(nahoda) {
+    nahoda = Math.floor(Math.random() * 8);
+    if (prvniMole === nahoda) {
+        while (nahoda === prvniMole) {
+            nahoda = Math.floor(Math.random() * 8);
+        }
+
+    }
+    prvniMole = nahoda;
+    return nahoda;
+}
+
+function clear() {
+    count = 30;
+    prvniMole = -1;
+    Skore = 1;
+    startt = false;
+}
+function zmiz() {
+    Mole.style.animation = "PohybDolu 0.3s 1"
+    Mole.style.top = "-20%";
+    setTimeout(funkce, 300);
+}
+function funkce() {
+    Mole.style.display = "none";
+}
